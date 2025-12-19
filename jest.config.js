e2e8d7eba5047ebf41f11199150adbe22bb9213b
Clayton <blockchainclay@gmail.com>
2025-12-19 00:02:45 -0500

    Add worker-based syllable analysis (#52)

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,18 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testRegex: ['tests/.*\\.(spec|test)\\.[tj]sx?$'],
   moduleNameMapper: {
+    '^@/workers/createAnalysisWorker$': '<rootDir>/src/workers/createAnalysisWorker.mock.ts',
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   globals: {
     'ts-jest': {
+      useESM: true,
       tsconfig: {
         jsx: 'react-jsx',
       },
